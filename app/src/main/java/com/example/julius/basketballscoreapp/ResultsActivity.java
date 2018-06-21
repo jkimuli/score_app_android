5d239b9b644012da9651e77dfbbee20d577f7cf1
Julius Kimuli <jkimuli@gmail.com>
2018-06-21 23:56:03 +0300

    Commit

---
+++ app/src/main/java/com/example/julius/basketballscoreapp/ResultsActivity.java
@@ -0,0 +1,15 @@
+package com.example.julius.basketballscoreapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class resultsActivity extends AppCompatActivity {
+
+    private
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+}
---
+++ app/src/main/java/com/example/julius/basketballscoreapp/data/ScoreDbHelper.java
@@ -0,0 +1,4 @@
+package com.example.julius.basketballscoreapp.data;
+
+public class ScoreDbHelper {
+}
